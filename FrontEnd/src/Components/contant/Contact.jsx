3312dde8332d97dc078e3ccd4b4c06db891d4beb
AboBakr <amrahmed682003@gmail.com>
2023-12-10 22:15:06 +0200

    zeyad

---
+++ FrontEnd/src/Components/contant/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './contact.css'
+function Contact(){
+    return(
+        <div className="contact">
+        <h1>contact Us</h1>
+        
+        <ul>
+            <li><a href="/">Facbook</a></li>
+            <li><a href="*">Twitter</a></li>
+            <li><a href="#">Whatsapp</a></li>
+        </ul>
+        </div>
+    );
+}
+export default Contact;
