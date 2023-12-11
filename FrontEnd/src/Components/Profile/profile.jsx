4b035e28281d07f2d0cd5e339541f33b0a8dbf24
ahmedsaber30 <146309815+ahmedsaber30@users.noreply.github.com>
2023-12-11 20:01:00 +0200

    Update profile.jsx

--- FrontEnd/src/Components/Profile/profile.jsx
+++ FrontEnd/src/Components/Profile/profile.jsx
@@ -3,16 +3,17 @@ import './ProfileComponent.css';
 //  import SignUP from '../SignUP/SignUP';
 
 const Profile = () => {
+  const userData = JSON.parse(localStorage.getItem('userData'));
   return (
     <div className='profile-container'>
-      <div className="profile-header">  <h1>Hi !</h1></div>
-      <div className="profile-details">
-      <p><strong>First Name:</strong></p>
-      <p><strong>Last Name:</strong> </p>
-      <p><strong>email:</strong> </p>
+      <div className="profile-header">  <h1>Hi {userData[0]} !</h1></div>
+      <div className="profile-details">     
+      <p><strong>Email: {userData[1]}</strong> </p>
+      <p><strong>PhoneNumber: {userData[2]}</strong></p>
+      <p><strong>age: {userData[3]}</strong> </p>
+      <p><strong>country: {userData[4]}</strong> </p>
     </div>
     </div>
   );
 };
-
 export default Profile; 
